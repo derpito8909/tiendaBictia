992b8f707eb71dac84864848ff4e1c08ac0ff01d
David Rodriguez <davisito544@gmail.com>
2020-11-02 21:16:06 -0500

    se agrega la ruta para actualizar y eliminar productos

--- backend/routes/productos.js
+++ backend/routes/productos.js
@@ -14,7 +14,8 @@ router.get("/lista", auth, async (req, res) => {
 	// si el usuario existe
 	const producto = await Productos.find({ idTendero: req.tendero._id });
 	res.send(producto);
-})
+});
+//ingresar un producto
 router.post("/", auth, async (req, res) => {
 	// se obtiene el id del tendero autenticado
 	const tendero = await Tendero.findById(req.tendero._id);
@@ -30,5 +31,37 @@ router.post("/", auth, async (req, res) => {
 	// se envia el resultado
 	const result = await producto.save();
 	res.status(200).send(result);
+});
+//actualizar producto
+router.put("/", auth, async (req, res) => {
+	const tendero = await Tendero.findById(req.tendero._id);
+	// si el tendero no existe
+	if (!tendero) return res.status(400).send("El tendero no existe en BD");
+	// si el usuario existe
+	const producto = await Productos.findByIdAndUpdate(
+		req.body._id,
+		{
+			idTendero: req.tendero._id,
+			nombre: req.body.nombre,
+			tipo: req.body.tipo,
+			precio: req.body.precio,
+		},
+		{
+			new: true,
+		}
+	);
+	if (!producto) return res.status(400).send("no hay producto asignada a este usuario");
+	res.status(200).send(producto);
+});
+// Eliminar Actividad
+router.delete("/:_id", auth, async (req, res) => {
+	//Buscamos el usuario
+	const tendero = await Tendero.findById(req.tendero._id);
+	// error si no se encuentra el usuario
+	const producto = await Productos.findByIdAndDelete(req.params._id);
+	// si no existe esa actividad
+	if (!producto) return res.status(400).send("No se encontro producto para eliminar");
+	// si se elimina el mensaje
+	res.status(200).send({ message: "Producto eliminado" });
 });
 module.exports = router;
